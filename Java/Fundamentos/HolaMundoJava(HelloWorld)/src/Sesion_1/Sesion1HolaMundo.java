3f978e6a4ec0fd8e269b498e452fb4408109f8bb
Alex <beccialex2002@gmail.com>
2022-07-19 22:44:19 -0300

    Sesion_2

---
+++ Java/Fundamentos/HolaMundoJava(HelloWorld)/src/Sesion_1/Sesion1HolaMundo.java
@@ -0,0 +1,13 @@
+package Sesion_1;
+
+//Mi clase en JAVA
+
+public class Sesion1HolaMundo {
+
+    public static void main(String args[]) {
+
+        System.out.println("Sesion 1!!!!");
+        System.out.println("Tarea 1:");
+        System.out.println("Hola mundo desde Java(Tarea)");
+    }
+}
--- Java/Fundamentos/HolaMundoJava(HelloWorld)/src/Sesion_2/Sesion_2.java
+++ Java/Fundamentos/HolaMundoJava(HelloWorld)/src/Sesion_2/Sesion_2.java
@@ -1,14 +1,9 @@
-//Mi clase en JAVA
+package Sesion_2;
 
-public class HolaMundo 
-{
-    public static void main(String args[])
-    {
-        /*
-        System.out.println("Sesion 1!!!!");
-        System.out.println("Tarea 1:");
-        System.out.println("Hola mundo desde Java(Tarea)");
-*/
+
+public class Sesion_2 {
+
+    public static void main(String args[]) {
         //SESION 2!!!!
         System.out.println("Sesion 2!!!");
         //Definicion de Variable
@@ -23,21 +18,23 @@ public class HolaMundo
         //Modificacion de la variable
         miVariableCadena = "Adios";
         System.out.println(miVariableCadena);
-        
+
         //var - interferencia entre los tipos en JAVA
         var miVariableEntera2 = 18;
         System.out.println(miVariableEntera2);
-        
+
         var miVariableCadena2 = "Nueva Cadena";
         //Concatenacion de Cadenas
         System.out.println("miVariableCadena2 = " + miVariableCadena2);
-        
+
         //Valores permitidos en el nombre de mis variables
-        
         var miVariable = 1;
         var _miVariable = 2;
         var $miVariable = 3;
+        var suma = miVariable + _miVariable + $miVariable;
+        System.out.println(suma);
         //var áVariable = 4; se puede pero no se recomienda utilizar
         // var #miVariable = 5; no se puede por el caracter especial.
+
     }
 }
