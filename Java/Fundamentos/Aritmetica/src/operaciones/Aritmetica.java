959cdd741682699492d4d09a9c68d52629fca94f
Alex <beccialex2002@gmail.com>
2022-07-26 13:27:44 -0300

    Seccion_9Finalizada

--- Java/Fundamentos/Aritmetica/src/operaciones/Aritmetica.java
+++ Java/Fundamentos/Aritmetica/src/operaciones/Aritmetica.java
@@ -3,9 +3,22 @@ package operaciones;
 
 public class Aritmetica 
 {
+    //Inicializando atributos
     int a;
     int b;
+    //Creando el constructor vacio!
+    public Aritmetica()
+    {
+        System.out.println("Ejecutando el constructor Vacio");
+    }
     
+    public Aritmetica(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+        System.out.println("Ejecutando el constructor con Argumentos");
+    }
+    //Metodos
     public void sumar(){
         int resultado = a + b;
         System.out.println("resultado = " + resultado);
--- Java/Fundamentos/Aritmetica/src/operaciones/PruebaAritmetica.java
+++ Java/Fundamentos/Aritmetica/src/operaciones/PruebaAritmetica.java
@@ -4,14 +4,22 @@ public class PruebaAritmetica {
     public static void main(String[] args) 
     {
         Aritmetica aritmetica1 = new Aritmetica();
-        aritmetica1.a = 5;
-        aritmetica1.b = 2;
-        aritmetica1.sumar();
-      
-        int resultado = aritmetica1.sumarConRetorno();
-        System.out.println("resultado con retorno'INT' = " + resultado);
+        System.out.println("aritmetica1 a: "+ aritmetica1.a);
+        System.out.println("aritmetica1 b: "+ aritmetica1.b);
         
-        resultado = aritmetica1.sumarConArgumentos(10,20);
-        System.out.println("resultado con argumentos = " + resultado);
+        Aritmetica aritmetica2 = new Aritmetica(10, 20);
+        System.out.println("aritmetica2 a: "+ aritmetica2.a);
+        System.out.println("aritmetica2 b: "+ aritmetica2.b);
+
+        //SECCION 8
+//        aritmetica1.a = 5;
+//        aritmetica1.b = 2;
+//        aritmetica1.sumar();
+//      
+//        int resultado = aritmetica1.sumarConRetorno();
+//        System.out.println("resultado con retorno'INT' = " + resultado);
+//        
+//        resultado = aritmetica1.sumarConArgumentos(10,20);
+//        System.out.println("resultado con argumentos = " + resultado);
     }
 }
